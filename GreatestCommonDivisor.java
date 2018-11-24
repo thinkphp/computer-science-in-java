63cd977a6a29ec39c65c2381f592dc805fd92cbc
Adrian Statescu <mergesortv@gmail.com>
2018-11-24 11:44:02 +0200

    I do not need HBox and Geometry

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -1,12 +1,10 @@
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 
