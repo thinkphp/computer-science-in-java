1420e18b917c781ebf929e8451f53f28a9d9ce58
Adrian Statescu <mergesortv@gmail.com>
2023-11-24 13:47:27 +0200

    Update ControlDigit.java

--- Algorithms /Basics/ControlDigit.java
+++ Algorithms /Basics/ControlDigit.java
@@ -1,20 +1,52 @@
+/*
+ * Author: Adrian Statescu.
+ * Description: Control Digit Algorithm.
+ * Date: __
+ * License: MIT
+ */
 import java.util.Scanner;
 
+public class Main {
 
-public class ControlDigit {
+    public static int Control1(int n) {
 
-    public static void main(String args[]) {
+           if(n % 9 != 0) {
 
-        int n;
+               return n % 9;
 
-        Scanner scan = new Scanner(System.in);
-        n  = scan.nextInt();
-        if(n%9!=0) {         
-           System.out.println(n%9); 
-        } else {
-           System.out.println(9); 
+           } else return 9;
+    }
+
+    public static int Control2(int n) {
+
+        int sum = 0;
+
+        while(n > 9 ) {
+
+            sum = 0;
+
+            while(n > 0) {
+
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
         }
- 
-        
-    } 
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+           System.out.println( "Give a n = " );
+
+           Scanner keyboard = new Scanner( System.in );
+
+           int n;
+
+           n = keyboard.nextInt();
+
+           System.out.println("Digit Control = " + Control2( n ));
+
+           System.out.println("Digit Control = " + Control1( n ));
+    }
 }
