a24e659634af3720b8ff5ddd1fc2893864509428
Adrian Statescu <mergesortv@gmail.com>
2023-12-07 11:04:36 +0200

    Update linearsearch.java

--- Algorithms /Search /Linearsearch /linearsearch.java
+++ Algorithms /Search /Linearsearch /linearsearch.java
@@ -1,1 +1,23 @@
+public class LinearSearch {
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i; // Element found, return its index
+            }
+        }
+        return -1; // Element not found in the array
+    }
 
+    public static void main(String[] args) {
+        int[] array = {10, 5, 7, 2, 8, 4, 1};
+        int targetElement = 8;
+
+        int result = linearSearch(array, targetElement);
+
+        if (result != -1) {
+            System.out.println("Element " + targetElement + " found at index " + result);
+        } else {
+            System.out.println("Element " + targetElement + " not found in the array");
+        }
+    }
+}
